a62f895f2fcb39481ed517b685e7e79cd5118e79
Granniesgotaids <luktay42@kingshigh.school.nz>
2022-03-22 09:58:23 +1300

    22/3/22 game1

--- Game1.py
+++ Game1.py
@@ -1,10 +1,10 @@
 from adventurelib import *
-
+current_room = carpark
 @when("drink juice")
 def juice():
 	print("You die")
 @when("shoot rabbit")
-def shoot_mitchell():
+def shoot_bunny
 	say("""You shoot the rabbit and he explodes""")
 @when("drive")
 @when("drive home")
@@ -12,6 +12,10 @@ def shoot_mitchell():
 def crash():
 	say("""You crash into a ditch and die,
 	 congratulations for completting this game""")
+def enter_house():
+	global current_room
+
+
 def main():
 	start()
 
