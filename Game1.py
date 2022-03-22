17770794bce3cf03486b42501b6b940bca9a6e1c
Granniesgotaids <luktay42@kingshigh.school.nz>
2022-03-23 11:00:34 +1300

    23/3/22

--- Game1.py
+++ Game1.py
@@ -1,21 +1,25 @@
 from adventurelib import *
-current_room = carpark
+carpark = Room("""You are in an empty parking lot, seems a bit dodgy
+but mostly alright. The lights glow dimmly""")
 @when("drink juice")
 def juice():
 	print("You die")
 @when("shoot rabbit")
-def shoot_bunny
+def shoot_bunny():
 	say("""You shoot the rabbit and he explodes""")
+
 @when("drive")
 @when("drive home")
 @when("crash in a ditch")
 def crash():
 	say("""You crash into a ditch and die,
 	 congratulations for completting this game""")
-def enter_house():
+def elevator():
 	global current_room
-
-
+current_room
+#check if door can be opened
+if current_room is not carppark:
+	say("No elevator to enter")
 def main():
 	start()
 
